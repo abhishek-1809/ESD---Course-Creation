d7f26ae000200a7ccd68cedb9e0da30717251c22
abhishek-1809 <abhisahu1920@gmail.com>
2024-11-23 17:01:13 +0530

    Update CourseScheduleRepo.java

--- src/main/java/com/abhishek/coursecreation/repo/CourseScheduleRepo.java
+++ src/main/java/com/abhishek/coursecreation/repo/CourseScheduleRepo.java
@@ -1,7 +1,8 @@
 package com.abhishek.coursecreation.repo;
 
 
+import com.abhishek.coursecreation.entity.Course_Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseScheduleRepo extends JpaRepository {
+public interface CourseScheduleRepo extends JpaRepository<Course_Schedule,Long> {
 }

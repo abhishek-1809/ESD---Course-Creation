5e77786c3140afae0d73ef525c49559689450b2c
abhishek-1809 <abhisahu1920@gmail.com>
2024-11-24 23:42:51 +0530

    Update Courses.java

--- src/main/java/com/abhishek/coursecreation/entity/Courses.java
+++ src/main/java/com/abhishek/coursecreation/entity/Courses.java
@@ -11,9 +11,10 @@ public class Courses {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "course_id")
-    private int courseId;
+    private Integer courseId;
 
-    @Column(name="course_code", nullable = false, unique = true)
+//    @Column(name="course_code", nullable = false, unique = true)
+@Column(name="course_code")
     private String courseCode;
 
     @Column(name="name")
@@ -23,17 +24,17 @@ public class Courses {
     private String description;
 
     @Column(name="year")
-    private int year;
+    private Integer year;
 
     @Column(name="term")
-    private int term;
+    private Integer term;
 
     @Column(name="faculty")
     private String faculty;
 
     @Column(name="credits")
-    private int credits;
+    private Integer credits;
 
     @Column(name="capacity")
-    private int capacity;
+    private Integer capacity;
 }

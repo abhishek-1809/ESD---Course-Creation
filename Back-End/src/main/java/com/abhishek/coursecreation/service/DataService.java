7daacb951bbd81961233e0f0407610640555b0b8
abhishek-1809 <abhisahu1920@gmail.com>
2024-11-26 20:46:01 +0530

    Create DataService.java

---
+++ Back-End/src/main/java/com/abhishek/coursecreation/service/DataService.java
@@ -0,0 +1,43 @@
+package com.abhishek.coursecreation.service;
+
+import com.abhishek.coursecreation.entity.Courses;
+import com.abhishek.coursecreation.entity.Specialisation;
+import com.abhishek.coursecreation.repo.CoursesRepo;
+import com.abhishek.coursecreation.repo.SpecialisationRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DataService {
+    @Autowired
+    SpecialisationRepo specialisationRepo;
+
+    @Autowired
+    CoursesRepo coursesRepo;
+
+
+    public List<String> getAllSpecialisationNames() {
+        List<Specialisation> specialisations = specialisationRepo.findAll();
+        List<String> specialisationNames = new ArrayList<>();
+
+        for (Specialisation specialisation : specialisations) {
+            specialisationNames.add(specialisation.getName());
+        }
+        return specialisationNames;
+    }
+
+    public List<String> getAllCourseNames() {
+        List<Courses> courses = coursesRepo.findAll();
+        List<String> courseNames = new ArrayList<>();
+
+        // Using a for loop to iterate through the specialisations
+        for (Courses course : courses) {
+            courseNames.add(course.getName());
+        }
+
+        return courseNames;
+    }
+}
